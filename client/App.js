0bf49de6bd09849ca9d5dcf6547cb9d67ee82ce2
RosendoTorres321 <rosendot@etainement.com>
2024-06-13 23:36:41 -0400

    Webpage Updated

---
+++ client/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Finances Webpage</h1>
+            {/* Add your webpage content here */}
+        </div>
+    );
+};
+
+export default App;
