cb808c5b365d33c8d3daa36203adbc1c3445b592
RosendoTorres321 <rosendot@etainement.com>
2024-06-18 23:00:17 -0400

    Name added to top of tables

--- src/components/ExpensesQuadrant.jsx
+++ src/components/ExpensesQuadrant.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Checkbox, TextField } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Checkbox, TextField, Typography } from '@mui/material';
 
 import formatDate from '../functions/formatDate';
 
@@ -24,74 +24,84 @@ const ExpensesQuadrant = ({ expensesData, onExpenseIncludeChange, onExpenseAmoun
 
     return (
         <div style={{ display: 'flex' }}>
-            <TableContainer component={Paper} style={{ width: '50%' }}>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Manual</TableCell>
-                            <TableCell>Amount</TableCell>
-                            <TableCell>Date</TableCell>
-                            <TableCell>Include</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {manualExpenses.map((expense) => (
-                            <TableRow key={expense.name}>
-                                <TableCell>{expense.name}</TableCell>
-                                <TableCell>
-                                    <TextField
-                                        type="number"
-                                        value={expense.amount}
-                                        onChange={(e) => handleAmountChange(expense, e.target.value)}
-                                    />
-                                </TableCell>
-                                <TableCell>{expense.date ? formatDate(expense.date) : ''}</TableCell>
-                                <TableCell>
-                                    <Checkbox
-                                        checked={expense.include}
-                                        onChange={() => handleIncludeChange(expense)}
-                                    />
-                                </TableCell>
+            <div style={{ width: '50%', padding: '0 8px' }}>
+                <Typography variant="subtitle1" component="h3" gutterBottom>
+                    Manual
+                </Typography>
+                <TableContainer component={Paper}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell>Amount</TableCell>
+                                <TableCell>Date</TableCell>
+                                <TableCell>Include</TableCell>
                             </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <TableContainer component={Paper} style={{ width: '50%' }}>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Recurring</TableCell>
-                            <TableCell>Amount</TableCell>
-                            <TableCell>Date</TableCell>
-                            <TableCell>Include</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {recurringExpenses.map((expense) => (
-                            <TableRow key={expense.name}>
-                                <TableCell>{expense.name}</TableCell>
-                                <TableCell>
-                                    <TextField
-                                        type="number"
-                                        value={expense.amount}
-                                        onChange={(e) => handleAmountChange(expense, e.target.value)}
-                                    />
-                                </TableCell>
-                                <TableCell>{expense.date ? formatDate(expense.date) : ''}</TableCell>
-                                <TableCell>
-                                    <Checkbox
-                                        checked={expense.include}
-                                        onChange={() => handleIncludeChange(expense)}
-                                    />
-                                </TableCell>
+                        </TableHead>
+                        <TableBody>
+                            {manualExpenses.map((expense) => (
+                                <TableRow key={expense.name}>
+                                    <TableCell>{expense.name}</TableCell>
+                                    <TableCell>
+                                        <TextField
+                                            type="number"
+                                            value={expense.amount}
+                                            onChange={(e) => handleAmountChange(expense, e.target.value)}
+                                        />
+                                    </TableCell>
+                                    <TableCell>{expense.date ? formatDate(expense.date) : ''}</TableCell>
+                                    <TableCell>
+                                        <Checkbox
+                                            checked={expense.include}
+                                            onChange={() => handleIncludeChange(expense)}
+                                        />
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </div>
+            <div style={{ width: '50%', padding: '0 8px' }}>
+                <Typography variant="subtitle1" component="h3" gutterBottom>
+                    Recurring
+                </Typography>
+                <TableContainer component={Paper}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell>Amount</TableCell>
+                                <TableCell>Date</TableCell>
+                                <TableCell>Include</TableCell>
                             </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+                        </TableHead>
+                        <TableBody>
+                            {recurringExpenses.map((expense) => (
+                                <TableRow key={expense.name}>
+                                    <TableCell>{expense.name}</TableCell>
+                                    <TableCell>
+                                        <TextField
+                                            type="number"
+                                            value={expense.amount}
+                                            onChange={(e) => handleAmountChange(expense, e.target.value)}
+                                        />
+                                    </TableCell>
+                                    <TableCell>{expense.date ? formatDate(expense.date) : ''}</TableCell>
+                                    <TableCell>
+                                        <Checkbox
+                                            checked={expense.include}
+                                            onChange={() => handleIncludeChange(expense)}
+                                        />
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </div>
         </div>
     );
 };
 
-export default ExpensesQuadrant;
+export default ExpensesQuadrant; 
