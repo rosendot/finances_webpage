6a33605fd7b80e619b139494d0365283de4ab4c3
RosendoTorres321 <rosendot@etainement.com>
2025-02-19 19:59:18 -0500

    delete old functions

--- src/functions/formatDate.js
+++
@@ -1,6 +0,0 @@
-const formatDate = (dateString) => {
-    const options = { month: '2-digit', day: '2-digit', year: 'numeric' };
-    return new Date(dateString).toLocaleDateString('en-US', options);
-};
-
-export default formatDate;
--- src/functions/isWithinWeek.js
+++
@@ -1,9 +0,0 @@
-const isWithinWeek = (date) => {
-    if (!date) return false;
-    const today = new Date();
-    const oneWeekFromNow = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
-    const itemDate = new Date(date);
-    return itemDate >= today && itemDate <= oneWeekFromNow;
-};
-
-export default isWithinWeek;
